2e1d4c85cdd7e8ea8f4d9be0e414b7d65161e05e
mysoyul <vega2k_@naver.com>
2021-09-02 13:28:12 +0900

    Member,Address 추가

---
+++ src/main/java/jpastudy/jpashop/domain/Member.java
@@ -0,0 +1,23 @@
+package jpastudy.jpashop.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Embedded;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+@Getter @Setter
+public class Member {
+    @Id @GeneratedValue
+    private Long id;
+
+    private String name;
+
+    @Embedded
+    private Address address;
+
+
+}
