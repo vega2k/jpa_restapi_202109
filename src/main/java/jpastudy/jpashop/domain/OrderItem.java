f73227c114de58ae1d53d9bc45fdaa6779546c4a
mysoyul <vega2k_@naver.com>
2021-09-02 14:47:09 +0900

    Item추가

--- src/main/java/jpastudy/jpashop/domain/OrderItem.java
+++ src/main/java/jpastudy/jpashop/domain/OrderItem.java
@@ -16,4 +16,13 @@ public class OrderItem {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "order_id")
     private Order order;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "item_id")
+    private Item item;
+
+    private int orderPrice;
+
+    private int count;
+
 }
