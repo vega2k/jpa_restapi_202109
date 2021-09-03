ad395b3172704c12a95caadc8423f91853b2f0f6
mysoyul <vega2k_@naver.com>
2021-09-03 15:57:43 +0900

    OrderSimpleController 수정

--- src/main/java/jpastudy/jpashop/controller/OrderSimpleApiController.java
+++ src/main/java/jpastudy/jpashop/controller/OrderSimpleApiController.java
@@ -21,6 +21,10 @@ public class OrderSimpleApiController {
     @GetMapping("/api/v1/simple-orders")
     public List<Order> ordersV1() {
         List<Order> all = orderRepository.findAllByQuerydsl(new OrderSearch());
+        for (Order order : all) {
+            order.getMember().getName(); //Lazy 강제 초기화
+            order.getDelivery().getAddress(); //Lazy 강제 초기화
+        }
         return all;
     }
 }
