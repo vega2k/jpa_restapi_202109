9b88089fa58e535c99cdde19b68955762ffd5469
mysoyul <vega2k_@naver.com>
2021-09-03 13:26:05 +0900

    Controller 수정

--- src/main/java/jpastudy/jpashop/controller/MemberApiController.java
+++ src/main/java/jpastudy/jpashop/controller/MemberApiController.java
@@ -1,13 +1,22 @@
 package jpastudy.jpashop.controller;
 
+import jpastudy.jpashop.domain.Member;
 import jpastudy.jpashop.service.MemberService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
 
 @RestController
 @RequiredArgsConstructor
 public class MemberApiController {
     private final MemberService memberService;
 
-
+    @PostMapping("/api/v1/member")
+    public Long saveMember(@RequestBody @Valid Member member) {
+        Long memberId = memberService.join(member);
+        return memberId;
+    }
 }
