e840496afd8ac92e2b44ece731351cc00340f392
mysoyul <vega2k_@naver.com>
2021-09-03 16:14:10 +0900

    OrderSimpleController 수정

--- src/main/java/jpastudy/jpashop/controller/OrderSimpleApiController.java
+++ src/main/java/jpastudy/jpashop/controller/OrderSimpleApiController.java
@@ -1,13 +1,18 @@
 package jpastudy.jpashop.controller;
 
+import jpastudy.jpashop.domain.Address;
 import jpastudy.jpashop.domain.Order;
+import jpastudy.jpashop.domain.OrderStatus;
 import jpastudy.jpashop.repository.OrderRepository;
 import jpastudy.jpashop.repository.OrderSearch;
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * xToOne(ManyToOne, OneToOne) 관계 최적화
@@ -27,4 +32,35 @@ public class OrderSimpleApiController {
         }
         return all;
     }
-}
+
+    /**
+     * V2. 엔티티를 조회해서 DTO로 변환
+     * - 단점: 지연로딩으로 쿼리 N번 호출
+     */
+    @GetMapping("/api/v2/simple-orders")
+    public List<SimpleOrderDto> ordersV2() {
+        List<Order> orders = orderRepository.findAllByQuerydsl(new OrderSearch());
+        List<SimpleOrderDto> result = orders.stream()   //Stream<Order>
+                .map(o -> new SimpleOrderDto(o))    //Stream<SimpleOrderDto>
+                .collect(Collectors.toList());  //List<SimpleOrderDto>
+        return result;
+    }
+
+    @Data
+    static class SimpleOrderDto {
+        private Long orderId;
+        private String name;
+        private LocalDateTime orderDate;
+        private OrderStatus orderStatus;
+        private Address address;
+        public SimpleOrderDto(Order order) {
+            orderId = order.getId();
+            name = order.getMember().getName(); //Lazy 강제 초기화
+            orderDate = order.getOrderDate();
+            orderStatus = order.getStatus();
+            address = order.getDelivery().getAddress(); //Lazy 강제 초기화
+        }
+    }
+
+
+}//class
