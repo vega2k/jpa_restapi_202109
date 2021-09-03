670a340be4ca5cf03c8754211b3513989cd52302
mysoyul <vega2k_@naver.com>
2021-09-03 10:42:21 +0900

    JPQL로 동적쿼리

--- src/main/java/jpastudy/jpashop/repository/OrderRepository.java
+++ src/main/java/jpastudy/jpashop/repository/OrderRepository.java
@@ -3,8 +3,10 @@ package jpastudy.jpashop.repository;
 import jpastudy.jpashop.domain.Order;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
+import org.springframework.util.StringUtils;
 
 import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
 import java.util.List;
 
 @Repository
@@ -25,4 +27,34 @@ public class OrderRepository {
         return em.createQuery("select o from Order o",Order.class)
                 .getResultList();
     }
+    //JPQL
+    public List<Order> findAllByString(OrderSearch orderSearch) {
+        String jpql = "select o From Order o join o.member m";
+        boolean isFirstCondition = true;
+        //주문 상태 검색
+        if (orderSearch.getOrderStatus() != null) {
+            if (isFirstCondition) {
+                jpql += " where";
+                isFirstCondition = false;
+            } else { jpql += " and"; }
+            jpql += " o.status = :status";
+        }
+        //회원 이름 검색
+        if (StringUtils.hasText(orderSearch.getMemberName())) {
+            if (isFirstCondition) {
+                jpql += " where";
+                isFirstCondition = false;
+            } else { jpql += " and"; }
+            jpql += " m.name like :name";
+        }
+        TypedQuery<Order> query = em.createQuery(jpql, Order.class).setMaxResults(1000);
+        if (orderSearch.getOrderStatus() != null) {
+            query = query.setParameter("status", orderSearch.getOrderStatus());
+        }
+        if (StringUtils.hasText(orderSearch.getMemberName())) {
+            query = query.setParameter("name", orderSearch.getMemberName());
+        }
+        return query.getResultList();
+    }
+
 }
