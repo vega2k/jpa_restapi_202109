889cc82959661cfa7998fd7022b286ebdeea7c19
mysoyul <vega2k_@naver.com>
2021-09-03 09:30:33 +0900

    biz메서드 추가

--- src/main/java/jpastudy/jpashop/domain/OrderItem.java
+++ src/main/java/jpastudy/jpashop/domain/OrderItem.java
@@ -1,7 +1,9 @@
 package jpastudy.jpashop.domain;
 
 import jpastudy.jpashop.domain.item.Item;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
@@ -9,6 +11,7 @@ import javax.persistence.*;
 @Entity
 @Table(name = "order_item")
 @Getter @Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class OrderItem {
     @Id @GeneratedValue
     @Column(name="order_item_id")
@@ -26,4 +29,23 @@ public class OrderItem {
 
     private int count;
 
+    //==주문생성 메서드==//
+    public static OrderItem createOrderItem(Item item, int orderPrice, int count) {
+        OrderItem orderItem = new OrderItem();
+        orderItem.setItem(item);
+        orderItem.setOrderPrice(orderPrice);
+        orderItem.setCount(count);
+        item.removeStock(count);
+        return orderItem;
+    }
+
+    //==비즈니스 로직 : 주문 취소 ==//
+    public void cancel() {
+        getItem().addStock(this.count);
+    }
+
+    //==비즈니스 로직 : 주문상품 전체 가격 조회 ==//
+    public int getTotalPrice() {
+        return getOrderPrice() * getCount();
+    }
 }
