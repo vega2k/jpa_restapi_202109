356d3bf6cbfe103d9b393f436188166467238d0a
mysoyul <vega2k_@naver.com>
2021-09-03 10:48:01 +0900

    JPQL로 동적쿼리

--- src/main/java/jpastudy/jpashop/repository/OrderRepository.java
+++ src/main/java/jpastudy/jpashop/repository/OrderRepository.java
@@ -1,5 +1,6 @@
 package jpastudy.jpashop.repository;
 
+import jpastudy.jpashop.domain.Member;
 import jpastudy.jpashop.domain.Order;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
@@ -7,6 +8,8 @@ import org.springframework.util.StringUtils;
 
 import javax.persistence.EntityManager;
 import javax.persistence.TypedQuery;
+import javax.persistence.criteria.*;
+import java.util.ArrayList;
 import java.util.List;
 
 @Repository
@@ -36,7 +39,9 @@ public class OrderRepository {
             if (isFirstCondition) {
                 jpql += " where";
                 isFirstCondition = false;
-            } else { jpql += " and"; }
+            } else {
+                jpql += " and";
+            }
             jpql += " o.status = :status";
         }
         //회원 이름 검색
@@ -44,7 +49,9 @@ public class OrderRepository {
             if (isFirstCondition) {
                 jpql += " where";
                 isFirstCondition = false;
-            } else { jpql += " and"; }
+            } else {
+                jpql += " and";
+            }
             jpql += " m.name like :name";
         }
         TypedQuery<Order> query = em.createQuery(jpql, Order.class).setMaxResults(1000);
@@ -56,5 +63,30 @@ public class OrderRepository {
         }
         return query.getResultList();
     }
+    //JPA Criteria
+    public List<Order> findAllByCriteria(OrderSearch orderSearch) {
+        CriteriaBuilder cb = em.getCriteriaBuilder();
+        CriteriaQuery<Order> cq = cb.createQuery(Order.class);
+        Root<Order> o = cq.from(Order.class);
+        Join<Order, Member> m = o.join("member", JoinType.INNER); //회원과 조인
+        List<Predicate> criteria = new ArrayList<>();
+        //주문 상태 검색
+        if (orderSearch.getOrderStatus() != null) {
+            Predicate status = cb.equal(o.get("status"),
+                    orderSearch.getOrderStatus());
+            criteria.add(status);
+        }
+        //회원 이름 검색
+        if (StringUtils.hasText(orderSearch.getMemberName())) {
+            Predicate name =
+                    cb.like(m.<String>get("name"), "%" +
+                            orderSearch.getMemberName() + "%");
+            criteria.add(name);
+        }
+        cq.where(cb.and(criteria.toArray(new Predicate[criteria.size()])));
+        TypedQuery<Order> query = em.createQuery(cq).setMaxResults(1000); //최대 1000건
+        return query.getResultList();
+    }
+
 
 }
