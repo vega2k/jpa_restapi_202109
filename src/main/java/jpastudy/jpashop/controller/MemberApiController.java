313edfbf4495a54bbd79c94d1b755218c639bf0f
mysoyul <vega2k_@naver.com>
2021-09-03 13:37:37 +0900

    Controller 수정

--- src/main/java/jpastudy/jpashop/controller/MemberApiController.java
+++ src/main/java/jpastudy/jpashop/controller/MemberApiController.java
@@ -2,12 +2,14 @@ package jpastudy.jpashop.controller;
 
 import jpastudy.jpashop.domain.Member;
 import jpastudy.jpashop.service.MemberService;
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
+import javax.validation.constraints.NotEmpty;
 
 @RestController
 @RequiredArgsConstructor
@@ -19,4 +21,32 @@ public class MemberApiController {
         Long memberId = memberService.join(member);
         return memberId;
     }
+
+    /**
+     * 등록 V2: 요청 값으로 Member 엔티티 대신에 별도의 DTO를 받는다.
+     */
+    @PostMapping("/api/v2/members")
+    public CreateMemberResponse saveMemberV2(@RequestBody @Valid
+                                                     CreateMemberRequest request) {
+        Member member = new Member();
+        member.setName(request.getName());
+        Long id = memberService.join(member);
+        return new CreateMemberResponse(id);
+    }
+
+    @Data
+    static class CreateMemberRequest {
+        @NotEmpty
+        private String name;
+    }
+
+    @Data
+    static class CreateMemberResponse {
+        private Long id;
+        public CreateMemberResponse(Long id) {
+            this.id = id;
+        }
+    }
+
+
 }
